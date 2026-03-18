2e0d2bd1f8c57a44a780f0e2d4cf5f4d627e59ec
jaimegarciae62 <Jaimegarciae@Yahoo.com>
2026-03-18 18:05:41 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+// Service Worker - Casa Middleburg
+const CACHE_NAME = 'casa-middleburg-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/app.js'
+];
+
+// Instalación
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// Activación
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(names => {
+            return Promise.all(
+                names.filter(name => name !== CACHE_NAME)
+                     .map(name => caches.delete(name))
+            );
+        })
+    );
+});
+
+// Fetch - Offline first
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
